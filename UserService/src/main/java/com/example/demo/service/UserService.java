8e235101e18ae60135461d54cf20dc6996eb5d04
dheeru177 <dheeru177@gmail.com>
2023-06-05 14:17:50 +0530

    Lec 3 complete

--- UserService/src/main/java/com/example/demo/service/UserService.java
+++ UserService/src/main/java/com/example/demo/service/UserService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
 
+import com.example.demo.entity.Rating;
 import com.example.demo.entity.User;
 import com.example.demo.repo.UserRepo;
 
@@ -15,6 +19,9 @@ public class UserService {
 	
 	@Autowired
 	UserRepo userRepo;
+	
+	@Autowired
+	RestTemplate restTemplate;
 
 	@PostMapping("/addUser")
 	public void addUser(User user) {
@@ -32,7 +39,21 @@ public class UserService {
 
 	public User getUserById(int userId) {
 		// TODO Auto-generated method stub
-		return userRepo.findById(userId).get();
+		 User user = userRepo.findById(userId).get();
+		try {
+		  ArrayList<Rating> array = restTemplate.getForObject("http://localhost:9094/rating/getRatingByUserId/2", ArrayList.class);
+	
+		
+	      user.setRating(array);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("exception");
+			System.out.println(user.getRating());
+		}
+       
+        
+        return user;
 	}
 	
 	
